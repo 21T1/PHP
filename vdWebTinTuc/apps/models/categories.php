e294ad07df52957d7a9e19e6d87fcfd3fa84d729
Anh <21T1hoanganh@gmail.com>
2023-09-08 23:21:51 +0700

    Update categories.php

--- vdWebTinTuc/apps/models/categories.php
+++ vdWebTinTuc/apps/models/categories.php
@@ -1,4 +1,13 @@
 <?php
     class apps_models_categories extends apps_libs_dbConnection{
         protected $tableName = 'categories';
+    
+        public function buildSelectBox(){
+            $query = $this->buildQueryParams(["SELECT"=>"id, name"])->select();
+            $result = [""=>"--select category--"];
+            foreach($query as $row)
+                $result[$row["id"]] = $row["name"];
+            return $result;
+        }
     }
+?>    
