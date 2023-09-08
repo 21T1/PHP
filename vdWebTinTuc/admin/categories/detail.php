b9dc9503e800d6c04ece4c6a8953d7ae3420b28d
Anh <21T1hoanganh@gmail.com>
2023-09-08 23:22:20 +0700

    Create detail.php

---
+++ vdWebTinTuc/admin/categories/detail.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Category Detail
+ */
+    $categories = new apps_models_categories();
+    $user = new apps_libs_userIdentity();
+    $router = new apps_libs_router();
+    
+    $id = intval($router->getGET("id"));
+    
+    //
+    if($id){
+        // SELECT * FROM categories WHERE id = $id LIMIT 1
+        $cateDetail = $categories->buildQueryParams([
+            "WHERE"=>"id=:id",
+            "params"=>[":id"=>$id]
+        ])->selectOne();
+        if(!$cateDetail)
+            $router->pageNotFound();
+    }else
+        $cateDetail = [
+            "id"=>"",
+            "name"=>""
+        ];   
+    //Add new category
+    if($router->getPOST("submit") && $router->getPOST("name")){
+        $params = [
+            ":name"=>$router->getPOST("name")
+        ];
+    
+        $result = false;
+        if($id){
+            $params[':id'] = $id;
+            $result = $categories->buildQueryParams([
+                "value"=>"name=:name",
+                "WHERE"=>"id=:id",
+                "params"=>$params
+            ])->update();
+        } else
+            $result = $categories->buildQueryParams([
+                "field"=>"(name, created_by) VALUES (?, ?)",
+                "value"=>[$params[":name"], $user->getId()]
+            ])->insert();
+    
+        if($result)
+            $router->redirect('categories/index');
+        else
+            $router->pageError('Cannot update db');
+    }
+?>
+<html>
+    <body>
+        <div>
+            <p>Hi <?= $user->getSESSION('username')?>,</p> 
+            <p>Welcome to Demo, <a href="<?= $router->createUrl('logout')?>">Logout?</a></p>
+            <h1><?= $id ? "View " : "Create New " ?>Category: <?= $cateDetail["name"]?></h1>
+        </div>
+        <!-- Detail of category's id = $id  -->
+        <form action="<?php echo $router->createUrl('categories/detail', ["id"=>$cateDetail["id"]])?>" method="POST">
+            <input type="text" name="name" value="<?= $cateDetail["name"]?>"><br>
+            <input type="submit" name="submit" value="Add">
+            <input type="button" value="Cancel" onclick="window.location.href = '<?= $router->createUrl("categories/index")?>'">
+        </form>
+    </body>
+</html>
