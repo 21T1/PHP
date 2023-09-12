3f51f325d31f2f7ef8eb36f892c199ecc9bf7866
Anh <21T1hoanganh@gmail.com>
2023-09-13 04:51:09 +0700

    Update delete.php

--- vdWebTinTuc/admin/users/delete.php
+++ vdWebTinTuc/admin/users/delete.php
@@ -20,15 +20,20 @@
             $router->pageError("Cannot delete this user")
 ?>
 <html>
+    <head>
+        <link rel="stylesheet" href="../CSS/delete.css">
+        <title>Delete User</title>
+        <?php include 'head.php'?>
+    </head>
     <body>
         <div>
-            <p>Hi <?= $user->getSESSION('username')?>,</p> 
-            <p>Welcome to Demo, <a href="<?= $router->createUrl('logout')?>">Logout?</a></p>
-            <h1>Do you want to delete <?= $userDetail["username"]?></h1>
+            <h1>Do you want to delete&ensp;<i><?= $userDetail["username"]?></i></h1>
         </div>
-        <form action="<?php echo $router->createUrl('users/delete', ["id"=>$id])?>" method="POST">
-            <input type="submit" name="submit" value="Yes">
-            <input type="button" value="No" onclick="window.location.href = '<?= $router->createUrl("users/index")?>'">
-        </form>
+        <div class="form">
+            <form action="<?php echo $router->createUrl('users/delete', ["id"=>$id])?>" method="POST">
+                <input id="btn" type="submit" name="submit" value="Yes">
+                <input id="btn" type="button" value="No" onclick="window.location.href = '<?= $router->createUrl("users/index")?>'">
+            </form>
+        </div>
     </body>
 </html>
