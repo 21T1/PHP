673f937a5e98e09bdf0058b45afb50d4a40d7fd6
Anh <21T1hoanganh@gmail.com>
2023-09-14 20:12:05 +0700

    Update index.php

--- vdWebTinTuc/public/index.php
+++ vdWebTinTuc/public/index.php
@@ -3,6 +3,9 @@ $router = new apps_libs_router();
 
 $page = $router->getGET("page") ? intval($router->getGET("page")) : 1;
 
+$currentCateId = "cate_id=".$router->getGET("cate_id");
+if($page!==1 &&  strpos($_SERVER['HTTP_REFERER'],$currentCateId) == false)
+        $page = 1;
 $posts = new apps_models_posts();
 $numPost = count($posts->buildQueryParams([
             'WHERE' => $router->getGET('cate_id') ? "cate_id = " . intval($router->getGET('cate_id')) : ""])->select());
@@ -24,7 +27,25 @@ $listPost = $posts->buildQueryParams([
             .link a:hover{
                 background-color: var(--effect-color);
             }
-            
+            #postDesc{
+                margin-top: 30px;
+                padding: 10px;
+            }
+            #postDesc img{
+                float: left;
+                width: 20%;
+                aspect-ratio: 1 / 1;
+                padding-right: 10px;
+                object-fit: cover;
+            }
+            #postDesc p{
+                padding: 0;
+                margin: 0;
+                text-align: justify;
+            }
+            #time{
+                float: right;
+            }
         </style>
     </head>
     <body>
@@ -33,10 +54,27 @@ $listPost = $posts->buildQueryParams([
         <?php
         foreach ($listPost as $row) {
             ?>
-            <li><a href="<?= $router->createUrl('../public/postDetail', ['id' => $row['id'], "page" => $page]) ?>">
+            <li><a href="<?= $router->createUrl('../public/postDetail', ['id' => $row['id']]) ?>">
             <?= $row['name'] ?></a>
-                <p><i><?= $row['created_time']?></i></p>    
-            <p><?= $row['description'] ?></p>
+                <div id="postDesc">
+                    <?php
+                        $url = "http://localhost".$router->createUrl('../public/postDetail', ['id' => $row['id']]);
+                        $htmlString = file_get_contents($url);
+                        $htmlDom = new DOMDocument;
+                        @$htmlDom->loadHTML($htmlString);
+
+                        $imageTags = $htmlDom->getElementsByTagName('img');
+
+                        $extractedImages = array();
+                        foreach($imageTags as $imageTag){
+                            $imgSrc = $imageTag->getAttribute('src');
+                            break;
+                        }
+                    ?>
+                    <img src="<?=$imgSrc?>" alt="img">
+                    <p><?= $row['description'] ?></p>
+                </div>
+                <p id="time"><?= $row['created_time']?></p>    
             </li>
             <?php
         }
